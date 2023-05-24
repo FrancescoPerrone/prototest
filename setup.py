dc74126c72492ccd26eb4bb4a540ff8ab8604343
Francesco Perrone <francescoperr@gmail.com>
2023-05-24 13:49:34 +0100

    Packadge structure and test files (this is the first commit).

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prototest',
+    version='0.1.0',
+    author='Francesco Perrone',
+    author_email='francescoperr@gmail.com',
+    description='a Python package to handle data in condensed matter physics.',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
