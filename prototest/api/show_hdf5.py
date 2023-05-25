e99c3b9a7afbd8e362bfdb624b8195fc04b6e358
Francesco Perrone <francescoperr@gmail.com>
2023-05-25 15:10:51 +0100

    Added some more handling for csv converter.

--- prototest/api/show_hdf5.py
+++ prototest/api/show_hdf5.py
@@ -2,39 +2,46 @@ import pandas as pd
 import h5py
 import numpy as np
 
-def hdf5_to_csv(hdf5_file, dataset_name, csv_file, axis_names=None):
+def hdf5_to_csv(hdf5_file, dataset_name, csv_file, axis_names=None, flatten=False):
     """
-    Reads a dataset from an HDF5 file, flattens the data to 2D if necessary,
-    and writes it to a CSV file.
+    Reads a dataset from an HDF5 file and writes it to a CSV file.
 
     Args:
     hdf5_file (str): Path to the HDF5 file.
     dataset_name (str): Name of the dataset in the HDF5 file.
     csv_file (str): Path to the output CSV file.
     axis_names (list of str): List of names for the axes of the data.
+    flatten (bool): If True, flatten the data to 2D. Default is False.
     """
     with h5py.File(hdf5_file, 'r') as f:
         data = f[dataset_name][...]
 
+        # Generate default axis names if not provided
         if axis_names is None:
             axis_names = [f'axis{i}' for i in range(data.ndim)]
-
-        # Flatten the data if it's more than 2D
-        if data.ndim > 2:
+        
+        if flatten:
+            # Flatten the data to 2D
             data = data.reshape(data.shape[0], -1)
-
-            # Generate column names
-            axis1_name = axis_names[1] if len(axis_names) > 1 else 'axis1'
-            axis2_name = axis_names[2] if len(axis_names) > 2 else 'axis2'
-            columns = [f'{axis1_name}{i}_{axis2_name}{j}'
-                       for i in range(data.shape[1])
-                       for j in range(data.shape[2])]
+            columns = [f'{axis_names[0]}_{i}_{axis_names[1]}_{j}'
+                        for i in range(data.shape[0])
+                        for j in range(data.shape[1])]
         else:
-            # For 2D data, use the second axis name for column names
-            columns = [f'{axis_names[1]}{i}' for i in range(data.shape[1])]
+            # Keep the original shape, generate a multi-index for columns
+            index = pd.MultiIndex.from_product([range(i) for i in data.shape[1:]], names=axis_names[1:])
+            columns = pd.Index(range(data.shape[0]), name=axis_names[0])
+            data = data.reshape(data.shape[0], -1)
+            df = pd.DataFrame(data, index=columns, columns=index)
+            df.to_csv(csv_file)
+            return
 
         df = pd.DataFrame(data, columns=columns)
         df.to_csv(csv_file, index=False)
+
+# To flatten the data and create custom names for each dimension
+# hdf5_to_csv('path_to_your_file.hdf5', 'dataset_name', 'output.csv', ['time', 'x', 'y'], flatten=True)
+# To keep the original shape and create custom names for each dimension
+# hdf5_to_csv('path_to_your_file.hdf5', 'dataset_name', 'output.csv', ['time', 'x', 'y'])
 
 
 
